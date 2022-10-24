a7e835f43d1f5e56a1b06fdf0f61ce2335b8acc3
Меднов Антон Павлович <amednov@neoflex.ru>
2022-10-24 14:18:55 +0300

    module8-task1 поправлена пунктуация

--- src/common/middlewares/types/i-documents-author.ts
+++ src/common/middlewares/types/i-documents-author.ts
@@ -1,5 +1,5 @@
 import {DocumentType} from '@typegoose/typegoose';
 
-export interface IDocumentAuthor <K extends string, T extends Record<K, {_id: unknown;} | undefined>>{
+export interface IDocumentAuthor <K extends string, T extends Record<K, {_id: unknown;} | undefined>> {
   get(documentId: string): Promise<DocumentType<T> | null>;
 }
